549efa91d509201c53422dd0a31da9a3bb8a7dba
1shaq-i <ishaqahmed123@hotmail.co.uk>
2025-08-27 15:44:43 +0100

    server changes and links updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,24 @@
-// 4EqO6JDhOqd4Y7eJ
 import express, { Express } from "express";
 import mongoose from "mongoose";
-import financialRecordRouter from './routes/financial-records'
+import financialRecordRouter from "./routes/financial-records";
+import cors from "cors";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
-const mongoURI: string = "mongodb+srv://ishaqahmed:4EqO6JDhOqd4Y7eJ@personalfinancetracker.8trwffj.mongodb.net/";
+const mongoURI: string =
+  "mongodb+srv://machadop1407:UvOUTpZW7EH85MZl@personalfinancetracker.qb0edtk.mongodb.net/";
 
-mongoose.connect(mongoURI).then(() => console.log("Connected to MongoDB")).catch((err) => console.error("Failed to connect to MongoDB:", err));
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
 
 app.use("/financial-records", financialRecordRouter);
 
 app.listen(port, () => {
-    console.log(`Server Running on Port ${port}`);
+  console.log(`Server Running on Port ${port}`);
 });
